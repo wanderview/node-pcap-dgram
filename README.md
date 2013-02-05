2f1ace35e58ae6efc4e0d4f6cab5f67da67e1fba
Ben Kelly <ben@wanderview.com>
2013-02-04 22:28:41 -0500

    Initial commit.  Stub out the dgram interface.

---
+++ README.md
@@ -0,0 +1,7 @@
+# pcap-dgram
+
+Mock UDP socket based on pcap file data.
+
+[![Build Status](https://travis-ci.org/wanderview/node-pcap-dgram.png)](https://travis-ci.org/wanderview/node-pcap-dgram)
+
+Work-in-progress.
---
+++ dgram.js
@@ -0,0 +1,61 @@
+// Copyright (c) 2013, Benjamin J. Kelly ("Author")
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'use strict';
+
+module.exports = PcapDgram;
+
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+util.inherits(PcapDgram, EventEmitter);
+
+function PcapDgram(pcapSource, address, opts) {
+  var self = (this instanceof PcapDgram)
+           ? this
+           : Object.create(PcapDgram.prototype);
+
+  opts = opts || {};
+
+  EventEmitter.call(self, opts);
+
+  return self;
+}
+
+// Event: message
+// Event: listening
+// Event: close
+// Event: error
+
+PcapDgram.prototype.send = function(buf, offset, length, port, address, callback) {};
+PcapDgram.prototype.bind = function(port, address) {};
+PcapDgram.prototype.close = function() {};
+PcapDgram.prototype.address = function() {};
+PcapDgram.prototype.setBroadcast = function(flag) {};
+PcapDgram.prototype.setTTL = function(ttl) {};
+PcapDgram.prototype.setMulticastTTL = function(ttl) {};
+PcapDgram.prototype.setMulticastLoopback = function(flag) {};
+PcapDgram.prototype.addMembership = function(mcAddress, mcInterface) {};
+PcapDgram.prototype.dropMembership = function(mcAddress, mcInterface) {};
+PcapDgram.prototype.unref = function() {};
+PcapDgram.prototype.ref = function() {};
